7b76b5568c705619299c68d2b7a148c5e8e52a3b
pcaicedo <pcaicedo@cidenet.com.co>
2019-09-02 15:17:55 -0500

    Se eliminan imports que ya no se utilizan.

--- src/test/java/com/kardex/core/test/ProductServiceTest.java
+++ src/test/java/com/kardex/core/test/ProductServiceTest.java
@@ -3,12 +3,8 @@ package com.kardex.core.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import com.kardex.core.model.Product;
 import com.kardex.core.services.ProductService;
 
